a3ed225752f0f250281db1edd6200bed6cb32847
Javiertqx <javiertanqx@phillip.com.sg>
2024-05-29 15:04:21 +0800

    added summary for deploy service

--- src/controllers/Token/token.controller.ts
+++ src/controllers/Token/token.controller.ts
@@ -2,12 +2,16 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { TokenService } from 'src/services/Token/token.service';
 import { TokenDto } from './token.dtos';
 import { BaseResponse } from 'src/base/base-response';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('token')
 export class TokenController {
   constructor(private readonly tokenService: TokenService) {}
 
   @Post('deploy')
+  @ApiOperation({
+    summary: 'Deploy tokens to testnet',
+  })
   async deployToken(@Body() dto: TokenDto): Promise<BaseResponse> {
     const token = await this.tokenService.deployToken(dto);
 

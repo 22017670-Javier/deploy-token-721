5105114ead3c1f990e7770ef07771e54360519c0
Javiertqx <javiertanqx@phillip.com.sg>
2024-05-29 17:23:58 +0800

    feat: transfer token

--- src/controllers/Token/token.controller.ts
+++ src/controllers/Token/token.controller.ts
@@ -1,6 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { TokenService } from 'src/services/Token/token.service';
-import { TokenDto } from './token.dtos';
+import { TokenDto, TokenTransferDto } from './token.dtos';
 import { BaseResponse } from 'src/base/base-response';
 import { ApiOperation } from '@nestjs/swagger';
 
@@ -20,5 +26,26 @@ export class TokenController {
       message: 'Token deployed successfully',
       data: { token },
     };
+  }
+
+  @Post('transfer')
+  @ApiOperation({
+    summary: 'Transfer tokens between accounts',
+  })
+  async TransferToken(@Body() dto: TokenTransferDto): Promise<BaseResponse> {
+    try {
+      const receipt = await this.tokenService.transfer(
+        dto.contractAddress,
+        dto.recipient,
+        dto.amount,
+      );
+      return {
+        success: true,
+        message: 'Token transferred successfully',
+        data: { receipt },
+      };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
